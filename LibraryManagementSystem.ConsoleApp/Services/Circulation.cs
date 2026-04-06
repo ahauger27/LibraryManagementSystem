cfa7f0a23e853d41a308864c99462d7d1007b2ae
Austin Hauger <114182613+ahauger27@users.noreply.github.com>
2026-04-06 11:25:11 -0400

    Started on Circulation class, notes

---
+++ LibraryManagementSystem.ConsoleApp/Services/Circulation.cs
@@ -0,0 +1,40 @@
+namespace LibraryManagementSystem.ConsoleApp.Services;
+
+public static class Circulation
+{
+    public static void CheckOutItem(Patron patron, Item item)
+    {
+        //if (GetItemCircStatus == "In")
+        //{
+        //    AddToActiveLoans(patron, item);
+        //}
+        //else
+        //{
+        //    Console.WriteLine("This item is unavailable to check out at this time");
+        //}
+        
+        //code here
+    }
+
+    public static void CheckInItem(Patron patron, Item item)
+    {    
+        
+        //code here
+    }
+
+    public static void AddToActiveLoans(Patron patron, Item item)
+    {
+        //code 
+    }
+
+    public static void RemoveFromActiveLoans(Patron patron, Item item)
+    {
+        //code
+    }
+
+    public static void GetItemCircStatus(Item item)
+    {
+        //return circStatus;
+    }
+}
+  
