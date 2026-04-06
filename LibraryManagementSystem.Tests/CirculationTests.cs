26c436a02b757249125226d239dd29525d6e31ac
Austin Hauger <114182613+ahauger27@users.noreply.github.com>
2026-04-06 09:53:00 -0400

    Add CirculationTests for checkout and checkin methods

---
+++ LibraryManagementSystem.Tests/CirculationTests.cs
@@ -0,0 +1,28 @@
+using LibraryManagementSystem.ConsoleApp.Models;
+using LibraryManagementSystem.ConsoleApp.Services;
+
+namespace LibraryManagementSystem.Tests
+
+public class CirculationTests
+{
+    public void CheckOutToPatron_Should_Successfully_Add_Available_Item_To_PatronActiveLoans()
+    {
+        //Code goes here
+    }
+
+    public void CheckOutToPatron_Should_Fail_To_Add_Unavailable_Item_To_PatronActiveLoans()
+    {
+        //Code goes here
+    }
+       
+    public void CheckInFromPatron_Should_Successfully_Make_Item_Available_Again()
+    {
+        //Code goes here
+    }
+
+    public void CheckInFromPatron_Should_Succeed_Even_If_Item_IsNotOut()
+    {
+        //Code goes here
+    }
+}
+  
